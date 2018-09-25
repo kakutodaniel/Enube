f584c5ec1fa5fc11880dbb32d2a09e825e62c925
Daniel <kakutodaniel@hotmail.com>
2018-09-25 09:15:19 -0300

    init

---
+++ ENube.Integrations.API/Startup.cs
@@ -0,0 +1,52 @@
+﻿using ENube.Integrations.Application;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ENube.Integrations
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            ENubeConfiguration.ConfigureServices(services, Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(opt => 
+            {
+                opt.SwaggerEndpoint("/swagger/v1.0/swagger.json", "V1.0");
+                opt.RoutePrefix = string.Empty;
+            });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
---
+++ ENube.Integrations.Application/ENubeConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Linq;
+
+namespace ENube.Integrations.Application
+{
+    public class ENubeConfiguration
+    {
+
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1.0", new Info { Title = "ENube Integrations API", Version = "v1.0" });
+
+                opt.DocInclusionPredicate((version, apiDescription) =>
+                {
+                    var values = apiDescription.RelativePath
+                        .Split('/')
+                        .Select(v => v.Replace("v{version}", version));
+
+                    apiDescription.RelativePath = string.Join("/", values);
+
+                    var versionParameter = apiDescription.ParameterDescriptions
+                        .SingleOrDefault(p => p.Name == "version");
+
+                    if (versionParameter != null)
+                        apiDescription.ParameterDescriptions.Remove(versionParameter);
+
+                    return true;
+                });
+            });
+
+            services.AddApiVersioning(opt =>
+            {
+                opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.ApiVersionReader = new MediaTypeApiVersionReader();
+
+            });
+
+
+        }
+
+    }
+}
