3b48ef2a64472212e279687e86444abffc19eb46
daniel.kakuto <daniel.kakuto@BRSAO1PW-2551>
2018-09-26 17:53:00 -0300

    crmService

--- ENube.Integrations.Application/Services/CRM/CRMService.cs
+++ ENube.Integrations.Application/Services/CRM/CRMService.cs
@@ -3,9 +3,11 @@ using ENube.Integrations.Application.Settings;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace ENube.Integrations.Application.Services.CRM
 {
@@ -13,44 +15,62 @@ namespace ENube.Integrations.Application.Services.CRM
     {
         protected readonly HttpClient _httpClient;
         protected readonly CRMSettings _crmSettings;
+        protected readonly ILogger<CRMService> _logger;
 
         public CRMService(
             HttpClient httpClient,
-            CRMSettings crmSettings)
+            CRMSettings crmSettings,
+            ILogger<CRMService> logger)
         {
             _httpClient = httpClient;
             _crmSettings = crmSettings;
+            _logger = logger;
         }
 
 
-        public async Task Post(PostRequest request)
+        public async Task<PostResponse> PostAsync(PostRequest request)
         {
+
+            var response = new PostResponse();
+
             try
             {
                 var payload = JsonConvert.SerializeObject(request);
 
-                var response = await _httpClient.PostAsync(
+                _logger.LogInformation($"Realizando POST no CRM - Payload: [{payload}]");
+
+                var result = await _httpClient.PostAsync(
                         _crmSettings.Endpoint,
                         new StringContent(payload, Encoding.UTF8, _crmSettings.ContentType)
                     );
 
-                response.EnsureSuccessStatusCode();
+                response.CodigoStatus = (int)result.StatusCode;
 
-                var result = response.Content;
-
+                if (result.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation($"POST no CRM realizado com SUCESSO - StatusCode: [{response.CodigoStatus}]");
+                    response.Sucesso = true;
+                }
+                else
+                {
+                    _logger.LogInformation($"POST no CRM realizado com ERRO - StatusCode: [{response.CodigoStatus}]");
+                    var headerStatus = result.Headers.GetValues("x-status-reason").FirstOrDefault();
+                    if (headerStatus != null)
+                    {
+                        var headerDataParse = JsonConvert.DeserializeObject<Dictionary<string, string>>(headerStatus);
+                        if (headerDataParse.ContainsKey("reason"))
+                        {
+                            response.Mensagem = headerDataParse["reason"];
+                        }
+                    }
+                }
             }
             catch (Exception ex)
             {
-
-                throw;
+                _logger.LogError(ex, $"ERRO ao realizar POST no CRM {_crmSettings.UrlBase}/{_crmSettings.Endpoint}");
             }
 
-
-
-
+            return response;
         }
-
-
-
     }
 }
--- ENube.Integrations.Application/Services/CRM/Contracts/PostResponse.cs
+++ ENube.Integrations.Application/Services/CRM/Contracts/PostResponse.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ENube.Integrations.Application.Services.CRM.Contracts
+﻿namespace ENube.Integrations.Application.Services.CRM.Contracts
 {
     public class PostResponse
     {
+        public int CodigoStatus { get; set; }
+
+        public bool Sucesso { get; set; }
+
+        public string Mensagem { get; set; }
+
     }
 }
